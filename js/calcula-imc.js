2842fe89fd7c48c8d217cf79a0e6e4986e532b27
annacllr <ana.fernandes.ferreira12@escola.pr.gov.br>
2025-12-08 07:52:03 -0300

    error de digitação calculalmc

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,90 +1,65 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
-/*
-console.log(paciente);  //tr
-console.log(tdPeso);    // td que tem peso
-console.log(tdAltura);  //td que tem altura
-console.log(peso);      //valor do peso
-console.log(altura);    //valor da altura
-*/
+botaoAdicionar.addEventListener("click", function(){
+    event.preventDefault();
+    var form = document.querySelector("#form-adiciona");
+    //Extrai informações do paciente do form
+    var paciente = obtemPacienteDoFormulario(form);
 
-var pacientes = document.querySelectorAll(".paciente");
+    //Cria a tr e a td do paciente
+    var pacienteTr = montaTr(paciente);
 
-for (var i=0; i < pacientes.length; i++) {
+    var tabela = document.querySelector("#tabela-pacientes");
 
-    var paciente = pacientes[i];      // inserir
+    tabela.appendChild(pacienteTr);
 
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
-    var tdImc = paciente.querySelector(".info-imc");
+    form.reset();
 
-    var pesoEhValido = validaPeso(peso);
-    var alturaEhValida = validaAltura(altura);
+});
 
-    if (!pesoEhvalido) {
-        console.log("Peso inválido");
-        pesoEhValido = false;
-        tdImc.textContent = "Peso inválido";
+// Dividir em tres funções para trabalhar com tarefas menores
+function obtemPacienteDoFormulario(form){
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+         imc: calculaImc(form.peso.value, form.altura.value)
     }
-
-
-    
-
-
-    if (!alturaEhValida){
-        console.log("Altura inválida");
-        alturaEhValida = false;
-        tdImc.textContent = "Altura inválida";
-    }
-
-
-    
-
-
-    if (alturaEhValida && pesoEhValido) 
-        var imc = calculaImc(peso,altura);
-        tdImc.textContent = imc;
-    }
-
+    return paciente;
 }
 
-// Reescrever o mesmo código em tres funções independentes
+function montaTr(paciente){
 
+    var pacienteTr = document.createElement("tr");
 
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
 
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.testContent = paciente.imc;
 
-
-function calculaImc(peso, altura){
-    var imc = 0;
-    imc = peso/(altura * altura);
-    return imc.toFixed(2);
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
 }
 
+function montaTd(paciente) {
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-imc"));
 
-
-
-function validaPeso(peso){
-    if (peso <= 0 ||  peso >= 1000) {
-        return false;
-    } else {
-      return true;
-    }
-}
-
-
-
-
-
-function validaAltura(altura) {
-    if (altura <=0 || altura > 3.00){
-        return false;
-    } else {
-        return true;
-    }
-
-
-}
+    return pacienteTr;
